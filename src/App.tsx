f7de3e2b521624a45340effae728e17384e2058a
nihalawasthi <nihalawasthi498@gmail.com>
2025-12-09 15:09:13 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import WindowManager from './components/WindowManager'
 import Taskbar from './components/Taskbar'
 import MultiWindowIndicator from './components/MultiWindowIndicator'
-import RemoteCursors from './components/RemoteCursors'
 
 export default function App() {
   return (
@@ -10,7 +9,6 @@ export default function App() {
       {/* Subtle grid pattern overlay */}
       <div className="absolute inset-0 bg-grid-pattern opacity-5"></div>
       <MultiWindowIndicator />
-      <RemoteCursors />
       <WindowManager />
       <Taskbar />
     </div>
